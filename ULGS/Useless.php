23c4c2021e2076ee7e9261511aa3ba972954ed74
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2020-12-06 11:57:52 -0500

    Create Useless.php

---
+++ ULGS/Useless.php
@@ -0,0 +1,624 @@
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
+<?php
+
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii()
+    {
+        return true;
+    }
+}
