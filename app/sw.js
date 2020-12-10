f5d08a03c5e9bfba47f0b2c141a0da3f61ff2bbd
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2020-12-10 14:31:26 -0500

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,21 +1,30 @@
-importScripts('/cache-polyfill.js');
-
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
-       '/styles/main.css',
-       '/scripts/main.min.js',
-       '/sounds/airhorn.mp3'
-     ]);
-   })
- );
+const version = "0.6.18";
+const cacheName = `airhorner-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/style11.css`,
+        `/script.js`,
+        
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
 });
-self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
