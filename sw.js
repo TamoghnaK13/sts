ffa9567e66e191b8586c5f8b307df4e5a1d5443b
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2021-02-05 14:50:02 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
- 
+
+const version = "0.6.18";
+const cacheName = `clearstreams-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/dmca.txt`
+       `/d.txt`,
+        `/404.html`,
+        `/script.js`,
+        `/sw.js`,
+        `/css/style.css`,
+        `/css/style2.css`,
+        `/manifest.json`,
+        `/favicon.ico`,
+        `/discord1.png`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
