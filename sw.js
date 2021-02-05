7f6e9bf9489c89f5b7a280fb94b81a0d3ee21e6f
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2021-02-05 15:28:55 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/styles/main.css',
+       '/scripts/main.min.js',
+       '/sounds/airhorn.mp3'
+     ]);
+   })
+ );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
